6d0dce4451ca013331cd0575894082f137e67b05
eduardomendezgomez <kevin.mendezgomez.2005@gmail.com>
2024-02-02 11:53:36 -0600

    creación del TabHost

--- CalculadoraBasica/app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ CalculadoraBasica/app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -9,14 +9,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
+        tbh = findViewById(id.tbhConversores);
+        tbh.setup();
 
+        tbh.addTab(tbh.newTabSpec("LOM").setContent(id.tabLongitud).setIndicator("LONGITUD",null));
+        tbh.addTab(tbh.newTabSpec("ALM").setContent(id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO",null));
+        tbh.addTab(tbh.newTabSpec("MON").setContent(id.tabMonedas).setIndicator("MONEDAS",null));
     }
 }
