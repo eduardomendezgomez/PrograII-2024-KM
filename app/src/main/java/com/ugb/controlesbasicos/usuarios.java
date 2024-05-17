9c53dffaeb1f2ba932e8fdfe223b447ea71a982e
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-05-16 21:08:14 -0600

    ya tiene tabhost

--- app/src/main/java/com/ugb/controlesbasicos/usuarios.java
+++ app/src/main/java/com/ugb/controlesbasicos/usuarios.java
@@ -3,7 +3,9 @@ package com.ugb.controlesbasicos;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,16 +15,25 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+
 public class usuarios extends AppCompatActivity {
     Button btn;
     TextView tempVal;
     DatabaseReference mDatabase;
     String miToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuarios);
-
+        btn = findViewById(R.id.btnGuardarUsuario);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(usuarios.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         obtenerToken();
     }
     private void obtenerToken(){
